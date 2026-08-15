b5e991c9f4d90b637e293692c61fc61f43f38460
Abdur Rehman <abdurrehman077.18@gmail.com>
2026-08-15 10:47:36 +0500

    Modify a commit

--- src/main/java/com/example/api/rest_api_database_crud/StudentRepository.java
+++ src/main/java/com/example/api/rest_api_database_crud/StudentRepository.java
@@ -3,11 +3,10 @@ package com.example.api.rest_api_database_crud;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student, Integer>{
- 
-    // all methods declarations bcz implementation will be provided by hiberante
-    // at run time.
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+    // Methods are only declared here.
+    // Spring Data JPA provides their implementation at runtime.
     // Read by name method
     List<Student> findByName(String name);
 
