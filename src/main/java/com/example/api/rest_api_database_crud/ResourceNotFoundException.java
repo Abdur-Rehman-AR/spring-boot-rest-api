74b30e37f98685279cc7c4e07501552365da9c91
Abdur Rehman <abdurrehman077.18@gmail.com>
2026-08-20 14:30:21 +0500

    Our custom exception class

--- src/main/java/com/example/api/rest_api_database_crud/ResourceNotFoundException.java
+++ src/main/java/com/example/api/rest_api_database_crud/ResourceNotFoundException.java
@@ -1,9 +1,11 @@
 package com.example.api.rest_api_database_crud;
 
+// Creating a custom Exception as we want to give a specific meaning to a problem.
 // RuntimeException is the parent class of many runtime error
 public class ResourceNotFoundException extends RuntimeException {
 
-    public void studentNotFoundException(String message) {
+    // Calling this class's constructor
+    public ResourceNotFoundException(String message) {
         // super(message) calls a constructor of RuntimeException that accepts a
         // String. As we need to store this message anywhere.
         super(message);
