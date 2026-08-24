f1e1b13cd99cc6b4e74f84c673e4af23f8bc4073
Abdur Rehman <abdurrehman077.18@gmail.com>
2026-08-24 18:48:19 +0500

    Added the db call for pageable + sorting + filtering method

--- src/main/java/com/example/api/student_management_rest_api_database_crud/StudentRepository.java
+++ src/main/java/com/example/api/student_management_rest_api_database_crud/StudentRepository.java
@@ -1,13 +1,28 @@
 package com.example.api.student_management_rest_api_database_crud;
 
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
     // Methods are only declared here.
     // Spring Data JPA provides their implementation at runtime.
-    // Read by name method
+
+    // Read methods for normal Student List.
     List<Student> findByName(String name);
+
+    List<Student> findByNameAndAge(String name, Integer age);
+
+    List<Student> findByAge(Integer age);
+
+    // Read methods for Paged Student List.
+    Page<Student> findByName(String name, Pageable pageable);
+
+    Page<Student> findByNameAndAge(String name, Integer age, Pageable pageable);
+
+    Page<Student> findByAge(Integer age, Pageable pageable);
 
 }
